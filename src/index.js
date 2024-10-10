148930e251f7361207c00ff2ec8dad22c1d3f825
LGH <diddl2002@duksung.ac.kr>
2024-10-10 09:41:53 +0900

    break-tests: n<0 부분 삭제함

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 // version 1 -> 2 -> 3
 function factorial(n) { // -> 5!
 
-    if( n < 0 ) {
-        throw new Error('n must be a positive integer');
-    }
+    // if( n < 0 ) {
+    //     throw new Error('n must be a positive integer');
+    // }
 
 
     if( n === 0 ) {
